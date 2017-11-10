4cae9fb7e2fe8902d8d936f8456bad18dfa96722
Logan Rakai <lmrakai@gmail.com>
2017-11-09 19:05:18 -0700

    Empty config placeholders for ease of instructions

--- front-end/js/config.js
+++ front-end/js/config.js
@@ -1,10 +1,10 @@
 (function (exports) {
     exports.appConfiguration = {
         userPool: new AmazonCognitoIdentity.CognitoUserPool({
-            UserPoolId: 'us-west-2_pcMj8UVXg',
-            ClientId: '2rv366qfb95oeccr6rgi4kndt3'
+            UserPoolId: '',
+            ClientId: ''
         }),
-        appUrl: 'https://muf30if8ji.execute-api.us-west-2.amazonaws.com/Prod'
+        appUrl: ''
     };
     
 })(window);
